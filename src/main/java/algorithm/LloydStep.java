22495ae3e8cd1f96374a03e4de4c3409c16efab0
Corvin <gridulin@gmail.com>
2021-07-07 01:00:46 +0200

    boarder edges fix

--- src/main/java/algorithm/LloydStep.java
+++ src/main/java/algorithm/LloydStep.java
@@ -103,8 +103,8 @@ public class LloydStep {
 			if (inputNodes.size() == 0) {
 				return ends;
 			}
-			for (var node: inputNodes)
-				if (findAdjacent(node).size()==1)
+			for (var node : inputNodes)
+				if (findAdjacent(node).size() == 1)
 					ends.add(node);
 			var nodesInOrder = new ArrayList<Node>();
 			Node lastNode = null;
@@ -221,10 +221,12 @@ public class LloydStep {
 			this.x = x;
 			this.y = y;
 		}
-		public void rescale(double offset, double scale){
-			x=(x-offset)*scale;
-			y=(y-offset)*scale;
+
+		public void rescale(double offset, double scale) {
+			x = (x - offset) * scale;
+			y = (y - offset) * scale;
 		}
+
 		public Node() {
 			this("");
 		}
@@ -248,18 +250,17 @@ public class LloydStep {
 		}
 
 		public void rescale(int width, int height, int pad) {
-			var offset = Math.min(nodes.stream().mapToDouble(n->n.x).min().orElse(0),
-					                     nodes.stream().mapToDouble(n->n.y).min().orElse(0))+pad;
-			var scale = Math.min(width/(nodes.stream().mapToDouble(n->n.x).max().orElse(width)+2*offset),
-				       			height/(nodes.stream().mapToDouble(n->n.y).max().orElse(height)+2*offset));
+			var offset = Math.min(nodes.stream().mapToDouble(n -> n.x).min().orElse(0),
+					nodes.stream().mapToDouble(n -> n.y).min().orElse(0)) + pad;
+			var scale = Math.min(width / (nodes.stream().mapToDouble(n -> n.x).max().orElse(width) + 2 * offset),
+					height / (nodes.stream().mapToDouble(n -> n.y).max().orElse(height) + 2 * offset));
 
 //			System.out.println(offset+" "+scale);
-			for (var node: nodes){
-				node.rescale(-offset,scale);
+			for (var node : nodes) {
+				node.rescale(-offset, scale);
 			}
 		}
 	}
-
 
 	private static final double WIDTH = 1000;
 	private static final double HEIGHT = 1000;
@@ -273,7 +274,7 @@ public class LloydStep {
 
 		delaunayTriangles = computeDelaunayTriangulation(inputGraph.nodes);
 
-		//List to keep track of outgoing edges
+		// List to keep track of outgoing edges
 		var outgoingEdges = new ArrayList<Edge>();
 		// compute voronoiCells
 		for (var delaunayEdge : getDelaunayEdges()) {
@@ -287,7 +288,7 @@ public class LloydStep {
 				var dx = delaunayEdge.from.x - delaunayEdge.to.x;
 				var dy = delaunayEdge.from.y - delaunayEdge.to.y;
 
-				//rotate by 90°
+				// rotate by 90°
 				double h = dy;
 				dy = dx;
 				dx = -h;
@@ -295,22 +296,22 @@ public class LloydStep {
 				double x;
 				double y;
 
-				//calculate the factor needed to reach the boarder in the x direction
+				// calculate the factor needed to reach the boarder in the x direction
 				if (dx > 0) {
 					x = (WIDTH - circumCenter.x) / dx;
 				} else {
 					x = -(circumCenter.x / dx);
 				}
-				
-				//calculate the factor needed to reach the boarder in the y direction
+
+				// calculate the factor needed to reach the boarder in the y direction
 				if (dy > 0) {
 					y = (HEIGHT - circumCenter.y) / dy;
 				} else {
 					y = -(circumCenter.y / dy);
 				}
 
-				//the smaller factor is used to calculate the intersection with the boarder
-				//since it is the direction in which the edge crosses the boarder first
+				// the smaller factor is used to calculate the intersection with the boarder
+				// since it is the direction in which the edge crosses the boarder first
 				if (Math.abs(x) < Math.abs(y)) {
 					dx *= Math.abs(x);
 					dy *= Math.abs(x);
@@ -319,7 +320,7 @@ public class LloydStep {
 					dy *= Math.abs(y);
 				}
 
-				//create new Edge and check clipping with other outgoing edges
+				// create new Edge and check clipping with other outgoing edges
 				var frameCrossing = new Node("F" + circumCenter.toString(), circumCenter.x + dx, circumCenter.y + dy);
 				var voronoiEdge = new Edge(circumCenter, frameCrossing);
 				getVoronoiCellForNode(delaunayEdge.from).edges.add(voronoiEdge);
@@ -333,51 +334,115 @@ public class LloydStep {
 				getVoronoiCellForNode(delaunayEdge.to).edges.add(voronoiEdge);
 			}
 		}
-		//close open voronoi cells, that have been clipped
-		for (var cell : voronoiCells){
+		// close open voronoi cells, that have been clipped
+		for (var cell : voronoiCells) {
+			var tl = new Node("tl", 0, 0);
+			var tr = new Node("bl", WIDTH, 0);
+			var bl = new Node("tl", 0, HEIGHT);
+			var br = new Node("bl", WIDTH, HEIGHT);
 			var nodes = cell.getNodesInOrder();
 			var start = nodes.get(0);
-			var end = nodes.get(nodes.size()-1);
-			if (cell.edges.stream().anyMatch(edge -> edge.from==start && edge.to == end || edge.from==end && edge.to == start)){
-				//closed loop nothing to be done
-			}else if (Math.abs(start.x-end.x)<0.01 || Math.abs(start.y-end.y)<0.01){
-				//start and end on one axis
-				cell.edges.add(new Edge(start,end));
-			}else{
-				//assuming only one edge crossing, TODO: handle multiple edge crossings
+			var end = nodes.get(nodes.size() - 1);
+			if (cell.edges.stream()
+					.anyMatch(edge -> edge.from == start && edge.to == end || edge.from == end && edge.to == start)) {
+				// closed loop nothing to be done
+			} else if (start.x < 1 && end.x < 1 || start.x > WIDTH - 1 && end.x > WIDTH - 1 || start.y < 1 && end.y < 1
+					|| start.y > HEIGHT - 1 && end.y > HEIGHT - 1) {
+				// start and end on one axis
+				cell.edges.add(new Edge(start, end));
+			} else if (start.x < 1 && end.x > WIDTH - 1 || start.x > WIDTH - 1 && end.x < 1) {
+				double y = 0;
+				for (var edge2 : cell.edges) {
+					var dx = edge2.to.x - edge2.from.x;
+					var dy = edge2.to.y - edge2.from.y;
+					var len = Math.sqrt(dx * dx + dy * dy);
+					dy /= len;
+					y += dy;
+				}
+				if (y > 0) {
+					if (start.x < end.x) {
+						cell.edges.add(new Edge(start, bl));
+						cell.edges.add(new Edge(bl, br));
+						cell.edges.add(new Edge(br, end));
+					} else {
+						cell.edges.add(new Edge(start, br));
+						cell.edges.add(new Edge(br, bl));
+						cell.edges.add(new Edge(bl, end));
+					}
+				} else {
+					if (start.x < end.x) {
+						cell.edges.add(new Edge(start, tl));
+						cell.edges.add(new Edge(tl, tr));
+						cell.edges.add(new Edge(tr, end));
+					} else {
+						cell.edges.add(new Edge(start, tr));
+						cell.edges.add(new Edge(tr, tl));
+						cell.edges.add(new Edge(tl, end));
+					}
+				}
+			} else if (start.y < 1 && end.y > HEIGHT - 1 || start.y > HEIGHT - 1 && end.y < 1) {
+				double x = 0;
+				for (var edge2 : cell.edges) {
+					var dx = edge2.to.x - edge2.from.x;
+					var dy = edge2.to.y - edge2.from.y;
+					var len = Math.sqrt(dx * dx + dy * dy);
+					dx /= len;
+					x += dx;
+				}
+				if (x > 0) {
+					if (start.y < end.y) {
+						cell.edges.add(new Edge(start, tr));
+						cell.edges.add(new Edge(tr, br));
+						cell.edges.add(new Edge(br, end));
+					} else {
+						cell.edges.add(new Edge(start, br));
+						cell.edges.add(new Edge(br, tr));
+						cell.edges.add(new Edge(tr, end));
+					}
+				} else {
+					if (start.y < end.y) {
+						cell.edges.add(new Edge(start, tl));
+						cell.edges.add(new Edge(tl, bl));
+						cell.edges.add(new Edge(bl, end));
+					} else {
+						cell.edges.add(new Edge(start, bl));
+						cell.edges.add(new Edge(bl, tl));
+						cell.edges.add(new Edge(tl, end));
+					}
+				}
+			} else {
+				// assuming only one edge crossing, TODO: handle multiple edge crossings
 
-				var tl=new Node("tl",0,0);
-				var tr=new Node("bl",WIDTH,0);
-				var bl=new Node("tl",0, HEIGHT);
-				var br=new Node("bl",WIDTH, HEIGHT);
 				Node corner = null;
 
-				if (start.x<0.01 || end.x<0.01) //left side
-					if (start.y<0.01 || end.y<0.01) //top side
-						corner=tl;
-					else //bottom side
-						corner=bl;
-				else //right side
-					if (start.y<0.01 || end.y<0.01) //top side
-						corner=tr;
-					else //bottom side
-						corner=br;
+				if (start.x < 0.01 || end.x < 0.01) // left side
+					if (start.y < 0.01 || end.y < 0.01) // top side
+						corner = tl;
+					else // bottom side
+						corner = bl;
+				else // right side
+				if (start.y < 0.01 || end.y < 0.01) // top side
+					corner = tr;
+				else // bottom side
+					corner = br;
 
-				cell.edges.add(new Edge(start,corner));
-				cell.edges.add(new Edge(corner,end));
+				cell.edges.add(new Edge(start, corner));
+				cell.edges.add(new Edge(corner, end));
 			}
 		}
 		for (var cell : voronoiCells) {
 			SutherlandHodgmanClipping.clip(cell, 0, 0, WIDTH, HEIGHT);
 		}
 	}
-	
+
 	public void checkClipping(ArrayList<Edge> edges, Edge newEdge) {
 		for (Iterator<Edge> i = edges.iterator(); i.hasNext();) {
 			Edge currentEdge = i.next();
-			//check if the current edge crosses the new edge
-			if (Line2D.linesIntersect(newEdge.from.x, newEdge.from.y, newEdge.to.x, newEdge.to.y, currentEdge.from.x, currentEdge.from.y, currentEdge.to.x, currentEdge.to.y) && !currentEdge.equals(newEdge) && !newEdge.from.equals(currentEdge.from)) {
-				//calculate the normed vectors of the current and new edge
+			// check if the current edge crosses the new edge
+			if (Line2D.linesIntersect(newEdge.from.x, newEdge.from.y, newEdge.to.x, newEdge.to.y, currentEdge.from.x,
+					currentEdge.from.y, currentEdge.to.x, currentEdge.to.y) && !currentEdge.equals(newEdge)
+					&& !newEdge.from.equals(currentEdge.from)) {
+				// calculate the normed vectors of the current and new edge
 				var dxNew = newEdge.from.x - newEdge.to.x;
 				var dyNew = newEdge.from.y - newEdge.to.y;
 				var dxCurrent = currentEdge.from.x - currentEdge.to.x;
@@ -388,18 +453,20 @@ public class LloydStep {
 				dyNew /= lenNew;
 				dxCurrent /= lenCurrent;
 				dyCurrent /= lenCurrent;
-				
-				//calculate the point at which the two edges cross
-				var x = ((currentEdge.from.x - newEdge.from.x) * dyCurrent - (currentEdge.from.y - newEdge.from.y) * dxCurrent) / (dxNew * dyCurrent - dyNew * dxCurrent);
-				var y = ((currentEdge.from.x - newEdge.from.x) * dyNew - (currentEdge.from.y - newEdge.from.y) * dxNew) / (dxNew * dyCurrent - dyNew * dxCurrent);
+
+				// calculate the point at which the two edges cross
+				var x = ((currentEdge.from.x - newEdge.from.x) * dyCurrent
+						- (currentEdge.from.y - newEdge.from.y) * dxCurrent) / (dxNew * dyCurrent - dyNew * dxCurrent);
+				var y = ((currentEdge.from.x - newEdge.from.x) * dyNew - (currentEdge.from.y - newEdge.from.y) * dxNew)
+						/ (dxNew * dyCurrent - dyNew * dxCurrent);
 				var intersec = new Node(newEdge.from.x + x * dxNew, newEdge.from.y + x * dyNew);
 				newEdge.to = intersec;
 				currentEdge.to = intersec;
-				
+
 				edges.remove(currentEdge);
 				edges.remove(newEdge);
-				
-				//calculate the intersection of the new outgoing Edge with the boarder
+
+				// calculate the intersection of the new outgoing Edge with the boarder
 				var dxOutgoing = -dxNew - dxCurrent;
 				var dyOutgoing = -dyNew - dyCurrent;
 
@@ -408,7 +475,7 @@ public class LloydStep {
 				} else {
 					x = -(newEdge.to.x / dxOutgoing);
 				}
-				
+
 				if (dyOutgoing > 0) {
 					y = (HEIGHT - newEdge.to.y) / dyOutgoing;
 				} else {
@@ -422,11 +489,11 @@ public class LloydStep {
 					dxOutgoing *= Math.abs(y);
 					dyOutgoing *= Math.abs(y);
 				}
-				
-				//create the new outgoing Edge
+
+				// create the new outgoing Edge
 				Edge newOutgoing = new Edge(intersec, new Node(newEdge.to.x + dxOutgoing, newEdge.to.y + dyOutgoing));
-				
-				//search for the voronoi cells of the new outgoing Edge
+
+				// search for the voronoi cells of the new outgoing Edge
 				boolean match1 = false;
 				boolean match2 = false;
 				for (var cell : voronoiCells) {
@@ -434,24 +501,26 @@ public class LloydStep {
 						var f = e.next();
 						if (f.equals(newEdge)) {
 							match1 = true;
-						}else if (f.equals(currentEdge)) {
+						} else if (f.equals(currentEdge)) {
 							match2 = true;
 						}
 					}
-					//if one of the new or current Edge is part of a cell the new outgoing Edge must also be part of that cell
+					// if one of the new or current Edge is part of a cell the new outgoing Edge
+					// must also be part of that cell
 					if (match1 ^ match2) {
 						cell.edges.add(newOutgoing);
 					}
 					match1 = false;
 					match2 = false;
 				}
-				//add the new outgoing Edge to the outgoing edges
+				// add the new outgoing Edge to the outgoing edges
 				edges.add(newOutgoing);
 				checkClipping(edges, newOutgoing);
 				return;
 			}
 		}
-		//if the Edge dosen't cross any other outgoing Edge it isn't changed and added to the outgoing Edges
+		// if the Edge dosen't cross any other outgoing Edge it isn't changed and added
+		// to the outgoing Edges
 		edges.add(newEdge);
 	}
 
